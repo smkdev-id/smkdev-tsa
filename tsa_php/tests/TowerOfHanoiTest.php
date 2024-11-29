4bae5dc6400824b7f61869ff2248749687a11fab
sapategu <adalgisoabercio@gmail.com>
2024-11-29 17:34:57 +0700

    feat(tsa_php) initial assessment

---
+++ tsa_php/tests/TowerOfHanoiTest.php
@@ -0,0 +1,9 @@
+<?php
+// tests/SumTest.php
+use PHPUnit\Framework\TestCase;
+
+class TowerOfHanoiTest extends TestCase {
+    public function testTowerOHanoi() {
+        require 'TowerOfHanoi.php';
+    }
+}
